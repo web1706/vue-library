25c69c7e0d9fd481475250470c88705e15d87269
cxy930123 <mail@xingyu1993.cn>
2019-11-01 09:16:59 +0800

    更新锁定页面滚动的机制

--- plugins/dialog/dialog-pagescroll.js
+++ plugins/dialog/dialog-pagescroll.js
@@ -2,11 +2,10 @@
  * 打开弹窗时调用`lock`锁定页面滚动条
  * 关闭弹窗时调用`unlock`解锁页面滚动条
  * 只有调用`unlock`的次数等于调用`lock`的次数时，
- * `lockCount`等于0，才真正解锁滚动条，
- * 因为此时才将所有弹窗关闭
- * 
+ * 才真正解锁滚动条，因为此时才将所有弹窗关闭
+ *
  * @example
- * 
+ *
  * //main.js
  * import pageScroll from 'dialog-pagescroll.js'
  * Vue.use(Dialog, {
@@ -17,7 +16,7 @@
  *     })
  *   ]
  * })
- * 
+ *
  * // App.vue
  * this.$dialog(<Component/>, {
  *   // 打开弹窗时是否禁用滚动，不传则使用默认值
@@ -28,27 +27,58 @@
 // 当前锁数量，大于0时禁止页面滚动
 let lockCount = 0;
 
-// 页面滚动条位置
-let offsetY = 0;
+// <body>原本的`overflow`和`paddingRight`属性
+let overflow = '';
+let paddingRight = '';
 
 // 加锁
 const lock = () => {
   if (lockCount++ === 0) {
-    offsetY = window.pageYOffset;
-    document.body.style.position = 'fixed';
-    document.body.style.top = -offsetY + 'px';
-    document.body.style.width = '100%';
+    const body = document.body
+    overflow = body.style.overflow
+    body.style.overflow = 'hidden'
+    // 将滚动条宽度加在`padding`上
+    const bodyStyle = getComputedStyle(body)
+    const bodyIsOverflow = document.documentElement.clientHeight < document.body.scrollHeight
+    const bodyOverflowY = bodyStyle.overflowY
+    const bodyHasScrollBar = bodyIsOverflow || bodyOverflowY === 'scroll'
+    if (bodyHasScrollBar) {
+      paddingRight = document.body.style.paddingRight
+      document.body.style.paddingRight = parseInt(bodyStyle.paddingRight) + getScrollBarWidth() + 'px'
+    }
   }
 }
 
 // 解锁
 const unlock = () => {
   if (--lockCount === 0) {
-    document.body.style.position = '';
-    document.body.style.top = '';
-    document.body.style.width = '';
-    window.scrollTo(0, offsetY);
+    document.body.style.overflow = overflow
+    document.body.style.paddingRight = paddingRight
   }
+}
+
+// 获取滚动条宽度
+const getScrollBarWidth = () => {
+  const outer = document.createElement('div')
+  const outerStyle = {
+    position: 'absolute',
+    top: '-9999px',
+    visibility: 'hidden',
+    width: '100px',
+    overflow: 'scroll'
+  }
+  Object.assign(outer.style, outerStyle)
+
+  const inner = document.createElement('div')
+  const innerStyle = {
+    width: '100%'
+  }
+  Object.assign(inner.style, innerStyle)
+
+  outer.append(inner)
+  document.body.append(outer)
+  setTimeout(() => outer.remove())
+  return outer.offsetWidth - inner.offsetWidth
 }
 
 /**
